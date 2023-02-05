058ef4514df54c071324f922ce71c7d5c52f5ab1
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-05 17:53:56 -0500

    server.js change

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ connectDB() ;
 const app = express() ;
 
 app.use(express.json()) ;
-app.use(cors({credentials: true, origin: 'http://localhost:3000'})) ;
+
 app.use(express.urlencoded({ extended: false })) ;
 app.use('/api/user', require('./routes/user.route')) ;
 
