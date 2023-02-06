1441e7677501fb6f1e3396027111f89591e9e596
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-06 13:55:33 -0500

    commit4

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ dotenv.config() ;
 app.use(cors()) ;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false })) ;
-app.use('/api/user', require(userRoutes)) ;
+app.use('/api/user', userRoutes) ;
 app.use(errorHandler) ;
 
 connectDB() ;
