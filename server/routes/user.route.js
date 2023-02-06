635490f5a40e6b2432cbce70b6fe610052b3bb3f
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-06 10:42:03 -0500

    commit

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,5 @@
 
-const express = require('express') ;
+import express from 'express' ;
 const router = express.Router() ;
 const { registerUser, loginUser, currentUser } = require('../controllers/user.controller') ;
 
