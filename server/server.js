be8ce69bec1f819c7148872945256210b260031e
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-05 19:42:43 -0500

    commit 3

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ const port =  process.env.PORT || 8000;
 connectDB() ;
 const app = express() ;
 
-app.use(express.json()) ;
 app.use(cors()) ;
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false })) ;
 app.use('/api/user', require('./routes/user.route')) ;
 
