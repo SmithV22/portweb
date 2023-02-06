81ebef4c1aed4d233bb75e2fec2498e81e249723
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-06 08:55:17 -0500

    commit

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 
-const express = require('express') ;
-const colors = require('colors') ;
-const cors = require('cors') ;
-const dotenv = require('dotenv').config() ;
+import express from 'express' ;
+import colors from 'colors' ;
+import cors from 'cors' ;
+import * as dotenv from 'dotenv' ;
 const { errorHandler } = require('./middleware/errorMiddleware') ;
 const userRoutes = require('./routes/user.route') ;
 const connectDB = require('./config/mongoose.config') ;
 
 const port =  process.env.PORT || 8000;
 
-connectDB() ;
+dotenv.config() ;
 const app = express() ;
+connectDB() ;
 
 app.use(cors()) ;
 app.use(express.json({ limit: '50mb' }));
