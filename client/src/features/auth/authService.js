c50e6e79763a701371ba04e4c1eefd477f883f2b
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-05 20:11:16 -0500

    commit

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios' ;
 
-const API_URL = 'http://localhost:5000/api/user' ;
+const API_URL = 'http://localhost:27016/api/user' ;
 
 const register = async (userData) => {
     const response = await axios.post(API_URL + '/register', userData)
