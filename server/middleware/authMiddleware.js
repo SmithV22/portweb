7c726361ede2d3c0a23da967b748d2abf3f53548
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-06 13:18:47 -0500

    commit4

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -23,6 +23,6 @@ const protect = asyncHandler(async (req, res, next) => {
         res.status(401)
         throw new Error('Not authorized, no token')
     }
-}) ;
+})
 
-module.exports = { protect } ;
+export default protect ; 
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,10 +3,10 @@ import express from 'express' ;
 const { registerUser, loginUser, currentUser } = require('../controllers/user.controller') ;
 
 const router = express.Router() ;
-const { protect } = require('../middleware/authMiddleware') ;
+const { protect } = '../middleware/authMiddleware' ;
 
 router.post('/register', registerUser) ;
 router.post('/login', loginUser) ;
 router.get('/currentUser', protect, currentUser ) ;
 
-module.exports = router ;
+export default router ;
