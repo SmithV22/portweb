1ce77bb03c4d032b894546b69c0c5483b1050ad2
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-09 12:44:33 -0500

    render commit

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
-import express from 'express' ;
-import colors from 'colors' ;
-import cors from 'cors' ;
-import * as dotenv from 'dotenv' ;
-import connectDB from '../server/config/mongoose.config.js' ;
-import errorHandler from './middleware/errorMiddleware.js' ;
+const express = require('express') ;
+const colors = require('colors') ;
+const cors = require('cors') ;
+const dotenv = require('dotenv').config() ;
+const { errorHandler } = require('./middleware/errorMiddleware') ;
+const connectDB = require('./config/mongoose.config') ;
+
 import userRoutes from './routes/user.route.js' ;
 
 const port =  process.env.PORT || 5000;
 connectDB() ;
 
 const app = express() ;
-dotenv.config() ;
 
 app.use(cors()) ;
 app.use(express.json({ limit: '50mb' }));
