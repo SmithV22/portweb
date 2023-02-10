ae89c152a588b7c25fbe87a4286c696f73080d6b
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-10 17:37:50 -0500

    render commit 4

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,12 +3,12 @@ const express = require('express') ;
 const { registerUser, loginUser, currentUser } = require('../controllers/user.controller') ;
 const protect = require('../middleware/authMiddleware') ;
 
-const router = express.Router() ;
-router.post('/register', registerUser) ;
-router.post('/login', loginUser) ;
-router.get('/currentUser', protect, currentUser ) ;
+const routerUser = express.Router() ;
+routerUser.post('/register', registerUser) ;
+routerUser.post('/login', loginUser) ;
+routerUser.get('/currentUser', protect, currentUser ) ;
 
 module.exports = {
-    router,
-    default: router, 
+    routerUser,
+    default: routerUser, 
 };
--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const express = require('express') ;
 const colors = require('colors') ;
 const cors = require('cors') ;
 const dotenv = require('dotenv').config() ;
+
+const { routerUser } = require('./routes/user.route.js') ;
 const { errorHandler } = require('./middleware/errorMiddleware') ;
 const connectDB = require('./config/mongoose.config') ;
 
-const { userRoutes } = require('./routes/user.route.js') ;
 
 const port =  process.env.PORT || 5000;
 connectDB() ;
@@ -15,7 +16,7 @@ const app = express() ;
 app.use(cors()) ;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false })) ;
-app.use('/api/user', require(userRoutes)) ;
+app.use('/api/user', require(routerUser)) ;
 app.use(errorHandler) ;
 
 
