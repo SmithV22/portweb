c5dd7b0d01f8ba4a2856341ed5e9b058e33c5434
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-10 19:05:37 -0500

    render commit4

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,12 +1,12 @@
 
 const express = require('express') ;
 const { registerUser, loginUser, currentUser } = require('../controllers/user.controller') ;
-const protect = require('../middleware/authMiddleware') ;
+const { protect } = require('../middleware/authMiddleware') ;
 
 const routerUser = express.Router() ;
 routerUser.post('/register', registerUser) ;
 routerUser.post('/login', loginUser) ;
-routerUser.get('/currentUser', protect, currentUser ) ;
+routerUser.post('/currentUser', protect, currentUser ) ;
 
 module.exports = {
     routerUser,
