04522306a97eba9471db768f8c749bda70098290
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-13 16:15:07 -0500

    render commit5

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -25,7 +25,4 @@ const protect = asyncHandler(async (req, res, next) => {
     }
 })
 
-module.exports = { 
-    protect,
-    default: protect, 
-    } ;
+export default protect ;
--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const cookieParser = require('cookie-parser') ;
 const { errorHandler } = require('./middleware/errorMiddleware') ;
 const { connectDB } = require('./config/mongoose.config') ;
 
-
 const port =  process.env.PORT || 5000;
 connectDB() ;
 
