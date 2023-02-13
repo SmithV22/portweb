1e574f26f28dff29f947d13cc01a26daec5604b4
Victoria Smith <89712186+SmithV22@users.noreply.github.com>
2023-02-13 16:04:12 -0500

    render commit

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors') ;
 const dotenv = require('dotenv').config() ;
 const cookieParser = require('cookie-parser') ;
 
-const { routerUser } = require('./routes/user.route.js') ;
+// const { routerUser } = require('../server/routes/user.route') ;
 const { errorHandler } = require('./middleware/errorMiddleware') ;
 const { connectDB } = require('./config/mongoose.config') ;
 
@@ -18,7 +18,7 @@ app.use(cors()) ;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false })) ;
 app.use(cookieParser()) ;
-app.use('/api/user', require(routerUser)) ;
+app.use('/api/user', require('./routes/user.route')) ;
 app.use(errorHandler) ;
 
 
